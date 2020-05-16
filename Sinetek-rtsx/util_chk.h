e8069be6a47efe49df3f309f24d32bb657cfe2fc
cholonam <cholonam@hotmail.com>
2020-05-16 11:24:36 +0900

    Separate util.h into util.h/util_chk.h/util_logging.h
    The reason is that some .cpp files are including headers from openbsd as
    C-code. These will include util.h, which includes some C++ definitions,
    causing compilation errors. Separating logging and check functions into
    smaller units will solve this.

---
+++ Sinetek-rtsx/util_chk.h
@@ -0,0 +1,24 @@
+#ifndef SINETEK_RTSX_UTIL_CHK_H
+#define SINETEK_RTSX_UTIL_CHK_H
+
+#include <IOKit/IOReturn.h> // kIOReturnSuccess
+
+#include "util_logging.h" // UTL_ERR
+
+#define UTL_CHK_PTR(ptr, ret) do { \
+	if (!(ptr)) { \
+		UTL_ERR("null pointer (%s) found!!!", #ptr); \
+		return ret; \
+	} \
+} while (0)
+
+#define UTL_CHK_SUCCESS(expr) \
+({ \
+	int sinetek_rtsx_utl_chk_success = expr; \
+	if (sinetek_rtsx_utl_chk_success != kIOReturnSuccess) { \
+		UTL_ERR("%s returns error %d", #expr, sinetek_rtsx_utl_chk_success); \
+	} \
+	sinetek_rtsx_utl_chk_success; \
+})
+
+#endif // SINETEK_RTSX_UTIL_CHK_H
