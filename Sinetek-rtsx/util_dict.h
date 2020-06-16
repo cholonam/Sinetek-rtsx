1f60aef837e723ee9da66c86593d5501372fd850
cholonam <cholonam@gmail.com>
2020-06-16 17:13:25 +0900

    Support dictionary locking

--- Sinetek-rtsx/util_dict.h
+++ Sinetek-rtsx/util_dict.h
@@ -1,35 +1,60 @@
 #pragma once
 
 #define UTL_STATIC_DICT_INIT(dict_name) \
+	template <> int                dict_name::lock = 0; \
 	template <> dict_name::key_t   dict_name::keys[] = { 0 }; \
 	template <> dict_name::value_t dict_name::values[] = { 0 }
 
 /*
  * This dictionary has the advantage that it does not need to be initialized at runtime.
  */
-template <typename T_KEY, typename T_VAL, unsigned ARRAY_SIZE = 10> class StaticDictionary {
+template <typename T_KEY, typename T_VAL, unsigned ARRAY_SIZE = 10, bool protect = true> class StaticDictionary {
+	static int lock;
 	static T_KEY keys[ARRAY_SIZE];
 	static T_VAL values[ARRAY_SIZE];
+
+	static inline void LOCK()
+	{
+		if (protect)
+			while (__sync_lock_test_and_set(&lock, 1)) {
+				__sync_synchronize();
+			}
+	}
+
+	static inline void UNLOCK()
+	{
+		if (protect)
+			__sync_lock_release(&lock);
+	}
 
 	// returns -1 if no slots available
 	static int getFreeSlot()
 	{
+		LOCK();
+		// here we own the lock
 		for (auto i = 0; i < ARRAY_SIZE; i++) {
 			if (keys[i] == 0) {
 				keys[i] = (T_KEY)-1; // mark as used
+				UNLOCK();
 				return i;
 			}
 		}
+		UTL_ERR("There are no free slots. Increase dictionary size.");
+		UNLOCK();
 		return -1; // no free slots
 	}
 
 	// returns -1 if not found
 	static int findKey(T_KEY key)
 	{
+		LOCK();
 		for (auto i = 0; i < ARRAY_SIZE; i++) {
-			if (keys[i] == key)
+			if (keys[i] == key) {
+				UNLOCK();
 				return i;
+			}
 		}
+		UNLOCK();
 		return -1;
 	}
 
@@ -53,7 +78,8 @@ public:
 		auto slotIdx = findKey(key);
 		if (slotIdx < 0)
 			return -1;
-		keys[slotIdx] = 0;
+		keys[slotIdx] = 0; // This frees the slot
+		__sync_synchronize(); // make sure it's visible
 		values[slotIdx] = 0;
 		return 0;
 	}
